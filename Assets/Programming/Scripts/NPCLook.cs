a8e35a1408e8cd21393b7e3fda187943299f8587
MKXScorpion <ananthpondichery62@gmail.com>
2022-05-05 15:17:11 +0100

    npc look  within range

--- Assets/Programming/Scripts/NPCLook.cs
+++ Assets/Programming/Scripts/NPCLook.cs
@@ -8,6 +8,11 @@ public class NPCLook : MonoBehaviour
     [SerializeField]
     private Transform Target;
 
+    [SerializeField]
+    private Transform TargetObject;
+
+    [SerializeField]
+    private float radius;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +22,24 @@ public class NPCLook : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {     
-       transform.LookAt(Target);
+    {
+        Collider[] hitcols = Physics.OverlapSphere(transform.position, radius);
+        foreach(var hitcol in hitcols)
+        {
+           if(hitcol.CompareTag("Player"))
+            {
+                transform.LookAt(Target);
+            }
+           else
+            {
+                transform.LookAt(TargetObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
