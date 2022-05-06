d46ee19f4c29bc43ddac363d85e15b71813e252d
MKXScorpion <ananthpondichery62@gmail.com>
2022-05-06 15:19:29 +0100

    outsidecam

--- Assets/Programming/Scripts/NPCLook.cs
+++ Assets/Programming/Scripts/NPCLook.cs
@@ -14,28 +14,31 @@ public class NPCLook : MonoBehaviour
     [SerializeField]
     private float radius;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider[] hitcols = Physics.OverlapSphere(transform.position, radius);
-        foreach(var hitcol in hitcols)
-        {
-           if(hitcol.CompareTag("Player"))
+  
+            Collider[] hitcols = Physics.OverlapSphere(transform.position, radius);
+            foreach (var hitcol in hitcols)
             {
-                transform.LookAt(Target);
-            }
-           else
-            {
-                transform.LookAt(TargetObject);
+                if (hitcol.CompareTag("Player"))
+                {
+                    transform.LookAt(Target);
+                }
+                else
+                {
+                    transform.LookAt(TargetObject);
+                }
             }
         }
-    }
 
     private void OnDrawGizmos()
     {
